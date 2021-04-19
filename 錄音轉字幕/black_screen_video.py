48cdce6c5d5f5c4bcc8b7d1f0961b7cd04bf1bfc
kantonesisch1234 <59163684+kantonesisch1234@users.noreply.github.com>
2021-04-19 11:39:43 +0800

    Add files via upload

---
+++ 錄音轉字幕/black_screen_video.py
@@ -0,0 +1,7 @@
+from text_preprocessing import *
+from moviepy.editor import *
+
+if __name__ == '__main__':
+    video = ColorClip((50,50), duration=1200,color=[0,0,0])
+    video.audio = AudioFileClip('text.mp3')
+    video.write_videofile('output.mp4',fps=10)
